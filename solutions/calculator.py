06c0b30675da60f7d7e83decbb9014826f5a98dd
mykhamill <mykhamill@gmail.com>
2014-09-19 23:47:02 +0100

    add calculator.py to the solutions folder and update README with Done

--- solutions/calculator.py
+++ solutions/calculator.py
@@ -5,105 +5,55 @@
 # A simple calculator to do basic operators. Make it a scientific calculator
 # for added complexity.
 
-# repl-calc
-# take input
-# parse input
-# compute result
-# output result
+def get_number_of_calculations():
+    return int(raw_input("Please enter the number of calculations you would like to perform (1-9):\n"))
 
-# valid operations:-
-# stage 1: +, -, *, /
-# stage 2: (, ), m, m+, m-,
-# stage 3: sqrt, ^, log, ln, !, e, pi
-# stage 4: rad, deg, sin, cos, tan
+def display_calculation_header(calc):
+    print "Calcuation ", calc + 1
 
-# valid input:-
-# stage 1: integers
-# stage 2: floats
-# stage 3: exp notation?
-# stage 4: other bases?
+def get_operation():
+    operation = {}
+    operation["operands"] = ["operand1", "operand2"]
+    op = raw_input("Please enter one of the following operation 'add', 'sub', 'mul', 'div':\n")
+    operation['calc'] = dict(zip(['add', 'sub', 'mul', 'div'],
+                                [lambda x, y: (x + y, "{x} + {y} =".format(x = x, y = y)),
+                                 lambda x, y: (x - y, "{x} - {y} =".format(x = x, y = y)),
+                                 lambda x, y: (x * y, "{x} * {y} =".format(x = x, y = y)),
+                                 lambda x, y: (x / y, "{x} / {y} =".format(x = x, y = y))]
+                                )
+                            )[op]
+    return operation
 
-# Stage 1 calculator
-import logging
-import readline
-import os
+def get_operands(operation):
+    for operand in operation["operands"]:
+        operation[operand] = int(raw_input("Please enter " + operand + ":\n"))
 
-LOG_FILENAME = "/tmp/cal.log"
-HISTORY_FILENAME = "/tmp/cal.hist"
+def display_result_of_calculation(operation, calc):
+    operation["result"], operation["disp"] = operation["calc"](operation["operand1"], operation["operand2"])
+    print "The result of calculation {calc}: {disp} {result}".format(calc = calc + 1,
+                                                                     result = operation["result"],
+                                                                     disp = operation["disp"])
 
-logging.basicConfig(filename = LOG_FILENAME,
-                    level=logging.DEBUG,)
+def ask_to_continue():
+    reply = raw_input("Do you wish to continue?[Y/N]:\n")
+    while reply.upper() not in ["Y", "N"]:
+        reply = raw_input("Please enter Y or N:\n")
+    if reply.upper() == "Y":
+        return True
+    else:
+        return False
 
-def get_history_item():
-  return [readline.get_history_item(i) for i in xrange(1, readline.get_current_history_length() + 1)]
+def main():
+    cont = True
+    operation = None
+    while cont:
+        calcs = get_number_of_calculations()
+        for calc in range(calcs):
+            display_calculation_header(calc)
+            operation = get_operation()
+            get_operands(operation)
+            display_result_of_calculation(operation, calc)
+        cont = ask_to_continue()
 
-add = lambda x, y: x + y
-sub = lambda x, y: x - y
-mul = lambda x, y: x * y
-div = lambda x, y: x / y
-
-class Main(object):
-  def __init__(self):
-    self.cont = True
-    self.operators = ['/', '*', '+', '-'] # operations in bodmas order
-    self.op_func = [div, mul, add, sub]
-    self.parsetree_root = []
-    self.prompt = " >: "
-    self.input = ""
-    self.output = ""
-    self.matches = []
-    if os.path.exists(HISTORY_FILENAME):
-      readline.read_history_file(HISTORY_FILENAME)
-    readline.set_completer(Main.complete)
-    readline.set_completion_display_matches_hook(Main.history_display)
-    readline.parse_and_bind("tab: complete")
-
-  def complete(self, text, state):
-    response = None
-    readline.insert_text(self.prompt)
-    if state == 0:
-      history_values = get_history_items()
-      logging.debug("history: {}".format(history_values))
-      if text:
-        self.matches = sorted(h for h in history_values if h and h.startswith(text))
-      else:
-        self.matches = []
-      logging.debug("matches: {}".format(self.matches))
-    try:
-      response = self.matches[state]
-    except IndexError:
-      response = None
-    logging.debug("complete({}{}) => {}".format(repr(text), state, repr(response)))
-    return response
-
-  def history_display(self, sub, matches, longest_match_length):
-    logging.debug("history_dispy: sub = %s, matches = %s, lml = %s" % (sub, repr(matches, longest_match_length)))
-
-  def read_input(self):
-    self.input = raw_input(self.prompt).strip()
-
-  def parse_calculation(self):
-    input = self.input
-    if input.lower() in ["quit", "q", "exit"]:
-      self.cont = False
-      return
-    output = "**" + input + "** being parsed\n Please wait...."
-    self.output = output
-
-  def display_output(self):
-    print '\n' + self.output
-
-  def main(self):
-    try:
-      while self.cont:
-        self.read_input()
-        self.parse_calculation()
-        if self.cont:
-          self.display_output()
-    finally:
-      readline.write_history_file(HISTORY_FILENAME)
-
-run = Main()
-main = run.main
 if __name__ == '__main__':
   main()
